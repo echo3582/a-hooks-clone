7bfa71e0e26491a473a22e67e2a89524b4a969c1
姽嫿酱 <qianlu3582@163.com>
2022-06-14 19:30:57 +0800

    模拟useState第4步:处理多个state第1步引入hooks数组和idx索引

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
 /**
  * 二、模拟useState
- * 
- * 处理多个state的情况，当前代码处理多个state会发生问题，因为现在全局变量不是数组，所以变量的值会被覆盖。
+ *
+ * 1.为了缓存多个state，引入hooks数组和索引idx。
+ * 2.render函数要把idx重新赋值为0，不然idx会一直增加，导致找不到对应的值。
+ * 3.useState函数要引入一个局部变量id来保存idx的值，因为setState会被单独调用，要保证setState被调用的时候id是准确的。
  * 
  */
 
- const React = (function() {
-    let _val
+ const React = (function () {
+    let hooks = [];
+    let idx = 0;
     function useState(initialVal) {
-        let state = _val || initialVal
-        const setState = newVal => _val = newVal
-        return [state, setState]
+      let id = idx;
+      let state = hooks[id] || initialVal;
+      const setState = (newVal) => (hooks[id] = newVal);
+      idx++;
+      return [state, setState];
     }
     function render(Component) {
-        let C = Component()
-        C.render()
-        return C
+      idx = 0;
+      let C = Component();
+      C.render();
+      return C;
     }
-    return { useState, render }
-  })()
+    return { useState, render };
+  })();
   
-  const Component = function() {
-    const [count, setCount] = React.useState(1)
-    const [text, setText] = React.useState('Apple')
+  const Component = function () {
+    const [count, setCount] = React.useState(1);
+    const [text, setText] = React.useState("Apple");
     return {
-        render: () => console.log({count, text}),
-        click: () => setCount(count + 1),
-        type: () => setText('Pear')
-    }
-  }
+      render: () => console.log({ count, text }),
+      click: () => setCount(count + 1),
+      type: () => setText("Pear")
+    };
+  };
   
-  var App = React.render(Component) // {count: 1, text: "Apple"}
-  App.click() 
-  var App = React.render(Component) // {count: 2, text: 2}
-  App.type() 
-  var App = React.render(Component) // {count: "Pear", text: "Pear"}
-  
-  
-  
+  var App = React.render(Component); // {count: 1, text: "Apple"}
+  App.click();
+  var App = React.render(Component); // {count: 2, text: "Apple"}
+  App.type();
+  var App = React.render(Component); // {count: 2, text: "Pear"}
+  App.click();
+  var App = React.render(Component); // {count: "3", text: "Pear"}
   
