04d8eab3c3c8889323016a4da202943fdc87e8ae
姽嫿酱 <qianlu3582@163.com>
2022-06-14 15:10:09 +0800

    模拟useState第3步:模拟函数组件

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 /**
  * 二、模拟useState
  * 
- * 把useState放到一个React模块里面
+ * 1.hooks通常是在函数组件里面使用，所以我们声明一个函数组件。
+ * 2.告诉React怎样渲染这个函数组件
+ * 3.把_val变量提到React模块顶层，使state不必是个函数
  * 
  */
 
 const React = (function() {
+    let _val
     function useState(initialVal) {
-        let _val = initialVal
-        let state = () => _val
+        let state = _val || initialVal
         const setState = newVal => _val = newVal
         return [state, setState]
     }
-    return { useState }
+    function render(Component) {
+        let C = Component()
+        C.render()
+        return C
+    }
+    return { useState, render }
 })()
 
-const [count, setCount] = useState(1)
-console.log(count()) // 1
-setCount(2)
-console.log(count()) // 2
+const Component = function() {
+    const [count, setCount] = React.useState(1)
+    return {
+        render: () => console.log(count),
+        click: () => setCount(count + 1)
+    }
+}
 
+var App = React.render(Component) // 1
+App.click() 
+
+var App = React.render(Component) // 2
+App.click()
+
+var App = React.render(Component) // 3
+App.click()
