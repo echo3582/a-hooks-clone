433f337bcea46a93edf265e3642b3d9e897b45d6
姽嫿酱 <qianlu3582@163.com>
2022-06-14 14:35:23 +0800

    模拟useState第1步

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 /**
  * 二、模拟useState
  * 
- * 输出不符合预期，因为setState并没有修改state的值。
+ * 把state修改成一个函数可以解决之前的问题，访问变量的时候需要调用函数，本例就是count()
  * 
  */
 
 function useState(initialVal) {
     let _val = initialVal
-    let state = _val
+    let state = () => _val
     const setState = newVal => _val = newVal
     return [state, setState]
 }
 
 const [count, setCount] = useState(1)
-console.log(count) // 1
+console.log(count()) // 1
 setCount(2)
-console.log(count) // 1
+console.log(count()) // 2
 
