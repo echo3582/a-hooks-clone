8d66ba26cc2932eb7a8c9c5ee18688b0a3426ad1
姽嫿酱 <qianlu3582@163.com>
2022-06-14 14:32:29 +0800

    模拟useState第0步

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 /**
- * 一、闭包
+ * 二、模拟useState
  * 
- * 闭包就是给函数创建私有变量的能力，比如下面这个例子，add函数的私有变量就是foo。
- * 外界可以通过它返回的函数来访问这个变量。
+ * 输出不符合预期，因为setState并没有修改state的值。
  * 
  */
 
-const add = (function() {
-    let foo = 1
-    return function() {
-        foo = foo + 1
-        return foo
-    }
-})()
+function useState(initialVal) {
+    let _val = initialVal
+    let state = _val
+    const setState = newVal => _val = newVal
+    return [state, setState]
+}
 
-console.log(add())// 2
-console.log(add())// 3
-console.log(add())// 4
-console.log(add())// 5
-console.log(add())// 6
+const [count, setCount] = useState(1)
+console.log(count) // 1
+setCount(2)
+console.log(count) // 1
+
