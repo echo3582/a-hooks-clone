873591f304103e026b587f3dd90697e5eef9a7bc
姽嫿酱 <qianlu3582@163.com>
2022-06-14 14:46:46 +0800

    模拟useState第2步:把useState放到React模块里面

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 /**
  * 二、模拟useState
  * 
- * 把state修改成一个函数可以解决之前的问题，访问变量的时候需要调用函数，本例就是count()
+ * 把useState放到一个React模块里面
  * 
  */
 
-function useState(initialVal) {
-    let _val = initialVal
-    let state = () => _val
-    const setState = newVal => _val = newVal
-    return [state, setState]
-}
+const React = (function() {
+    function useState(initialVal) {
+        let _val = initialVal
+        let state = () => _val
+        const setState = newVal => _val = newVal
+        return [state, setState]
+    }
+    return { useState }
+})()
 
 const [count, setCount] = useState(1)
 console.log(count()) // 1
